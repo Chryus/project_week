b4952ba5cdf2d4878b54ba52863c4cceed272b97
Chris <chris.haack@me.com>
2013-12-08 17:09:09 -0500

    refactor regex to eliminate extra zeros

--- time_util/time_util_challenge.rb
+++ time_util/time_util_challenge.rb
@@ -31,10 +31,9 @@ class Flight < Time
 	end
 
 	def parse_time
-		reg = /(\d{2})|(\d{1})/
+		reg = /(\d{1}):(\d{2})/
 		time_parse = arrivals.collect {|time| time.scan(reg)}.flatten
 		time_out_of_strings = time_parse.collect{|time| time.to_i}
-		time_out_of_strings.delete_if{|i| i == 0}
 	end
 
 	def hours_to_minutes
@@ -46,6 +45,7 @@ class Flight < Time
 	def add_extra_minutes
 		#need to convert this to odds
 		minutes = parse_time.select.each_with_index{|num, i| i.odd?}#values_at(1,3,5,7,9)
+		#this is a bit of ruby magic that adds indices from both arrays via .zip
   	hours_to_minutes.zip(minutes).map{|both| both.reduce(:+)}
 	end
 
@@ -60,16 +60,6 @@ class Flight < Time
 	def hours_remainder
 		(mean_minutes_to_hours - mean_minutes_to_hours.round(0)).abs
 	end
-
-	# def hours_remainder
-	# 	#
-	# 	if mean_minutes_to_hours.round(0) < mean_minutes_to_hours
-	# 		mean_minutes_to_hours - mean_minutes_to_hours.round(0)
-	# 	else
-	# 		mean_minutes_to_hours.round(0) - mean_minutes_to_hours
-	# 	end
-	# 	#else			#mean_minutes_to_hours - mean_minutes_to_hours.round(2)
-	# end
 
 	def hours_remainder_to_minutes
 		((hours_remainder * 100)*60/100).round(0).to_f/100	#take the remainder in hours and convert to minutes, round to the whole minute, and convert to decimal
@@ -130,7 +120,7 @@ class Flight < Time
 
 end
 
-flight = Flight.new(["6:41am", "6:51am", "7:01am"])
+flight = Flight.new(["1:30pm", "1:35pm", "2:51pm", "1:56pm", "2:01pm", "3:06pm", "2:00pm"])
 p flight.parse_time
 p flight.hours_to_minutes
 p flight.add_extra_minutes
@@ -141,4 +131,21 @@ p flight.hours_remainder_to_minutes
 p flight.add_minutes_to_hours
 p flight.add_minutes_to_hours.to_s
 p flight.return_flight
+
+
+["11:51pm", "11:56pm", "12:01am", "12:06am", "12:11am"]
+["8:30pm", "9:30pm", "11:51pm", "11:56pm", "12:01am", "12:06am", "12:11am"]
+["8:30am", "9:30am", "11:51am", "11:56am", "12:01pm", "12:06pm", "12:11pm"]
+["1:30pm", "1:35pm", "2:51pm", "1:56pm", "2:01pm", "3:06pm", "2:00pm"]
+
+	#lol this does the same thing as the absolute value method (.abs)
+	# def hours_remainder
+	# 	if mean_minutes_to_hours.round(0) < mean_minutes_to_hours
+	# 		mean_minutes_to_hours - mean_minutes_to_hours.round(0)
+	# 	else
+	# 		mean_minutes_to_hours.round(0) - mean_minutes_to_hours
+	# 	end
+	# end
+
+
 
