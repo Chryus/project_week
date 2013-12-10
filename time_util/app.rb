95ba7a9e16915f21d89b5fd46bc1eed7dd85eff9
Chris <chris.haack@me.com>
2013-12-09 21:55:15 -0500

    refactor regex in time_util

--- time_util/app.rb
+++ time_util/app.rb
@@ -1,136 +1,68 @@
 require './parser'
+require './lib/flight'
 require 'awesome_print'
 require 'debugger'
-
-#class FlightCalculator
-
-		#attr_reader :flight, :user_choice
 
 parser = Parser.new
 parser.make_flight_objects
 
-	#def initialize(user_choice=nil)
-	#	@user_choice = user_choice
-	#end
+class FlightChecker
 
-	# def flight=(user_choice)
-	# 	@flight = Flight.all.select{|flight| flight = user_choice}
-	# end
+	attr_accessor :user_choice
 
-	def format_flights
-		reg = /(\d{1}):(\d{2})/
-		time_parse = arrivals.collect {|time| time.scan(reg)}.flatten
-		time_out_of_strings = time_parse.collect{|time| time.to_i}
-	end
-
-	def hours_to_minutes
-		#need to conver this needs evens
-		hours = format_flights.select.each_with_index{|num, i| i.even?}#values_at(0,2,4,6,8)
-		hours.collect{|hour| hour * 60}
-	end
-
-	def add_extra_minutes
-		#need to convert this to odds
-		minutes = format_flights.select.each_with_index{|num, i| i.odd?}#values_at(1,3,5,7,9)
-		#this is a bit of ruby magic that adds indices from both arrays via .zip
-  	hours_to_minutes.zip(minutes).map{|both| both.reduce(:+)}
-	end
-
-	def mean_minutes
-		add_extra_minutes.reduce(:+)/arrivals.size
-	end
-
-	def mean_minutes_to_hours
-		 (mean_minutes.to_f/60).round(2)
-	end
-
-	def hours_remainder
-		(mean_minutes_to_hours - mean_minutes_to_hours.round(0)).abs
-	end
-
-	def hours_remainder_to_minutes
-		((hours_remainder * 100)*60/100).round(0).to_f/100	#take the remainder in hours and convert to minutes, round to the whole minute, and convert to decimal
-	end
-
-	def add_minutes_to_hours
-		hours_remainder_to_minutes + mean_minutes_to_hours.round(0)
-	end
-
-	def return_average_flight_time_midnight
-		reg = /(\.)/
-		if add_minutes_to_hours > 12
-			time = add_minutes_to_hours.to_s.gsub(reg, ":")
-			time<<"am"
-		else
-			time = add_minutes_to_hours.to_s.gsub(reg, ":")
-			time<<"pm"
-		end
-		time
-	end
-
-	def return_average_flight_time_midday
-		reg = /(\.)/
-		if add_minutes_to_hours < 12
-			time = add_minutes_to_hours.to_s.gsub(reg, ":")
-			time<<"am"
-		else
-			time = add_minutes_to_hours.to_s.gsub(reg, ":")
-			time<<"pm"
-		end
-		time
-	end
-
-	def return_average_flight_time
-		reg = /(\.)/
-		if arrivals[0].include?("am")
-			time = add_minutes_to_hours.to_s.gsub(reg, ":")
-			time<<"am"
-		else
-			time = add_minutes_to_hours.to_s.gsub(reg, ":")
-			time<<"pm"
-		end
-		time
-	end
-
-	def return_flight
-		average_time = nil
-		if arrivals[0].include?("pm") && arrivals[-1].include?("am") 
-			average_time = return_average_flight_time_midnight
-		elsif arrivals[0].include?("am") && arrivals[-1].include?("pm")
-			average_time = return_average_flight_time_midday
-		else
-			average_time = return_average_flight_time
-		end
-		average_time
+	def initialize(user_choice=nil)
+		@user_choice
 	end
 
 	def print_flights
-	  Flight.all.each do |flight|
-	    puts flight.number   
-	  end
-	  puts "There are #{Flight.count} flights to choose from.\n"
-	end
-
-	def print_all_arrival_times
-		puts "To see the average arrival time for any flight, please enter the flight number."
-		answer = gets.chomp.downcase
-		#FlightCalculator.new(answer)
-		if answer == flight.number
-			flight.return_flight
+		Flight.all.each do |flight|
+			puts "\nFlight #{flight.number} | Arrival times: #{flight.arrivals}\n\n"
 		end
 	end
-	
-	#def flight_program
-	puts "Hello, would you like to see a list of flights? (y/n) "
-	answer = gets.chomp.downcase
-		if answer == "y"
+
+	def wanna_see_flights?
+		puts "\nHello, would you like to see a list of flights? (y/n) "
+		user_choice = gets.chomp.downcase
+			if user_choice == "y"
 			print_flights
-			print_all_arrival_times
 		else
 			print "Good luck with your flight."
 		end
-	#end
-#end
+	end
 
-#one = FlightCalculator.new
-#one.flight_program
+	def which_flight?
+		#debugger
+		puts "\nPlease select a flight from the list (type in the flight number)"
+		user_choice = gets.chomp.to_i
+			#if user_choice.class == Fixnum
+		find_flight(user_choice)
+			# else
+			# 	puts "I do not understand"
+			# end
+	end
+
+	def find_flight(user_choice)
+		Flight.all.each do |flight| 
+			if flight.number == user_choice
+				ap "Flight #{flight.number} has an average arrival time of #{flight.return_flight}"
+			else
+			 	"I'm sorry that isn't a flight."
+			end
+		end
+	end
+
+	def app
+		wanna_see_flights?
+		which_flight?
+	end
+
+end
+
+ calc = FlightChecker.new
+ calc.app
+# Flight.all.each do |flight|
+# 	ap flight.number
+# end
+
+
+
