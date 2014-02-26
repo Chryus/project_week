a0b78cf325c03a78eae2a66580ae1f31879b96bc
Chris <chris.haack@me.com>
2014-02-25 19:50:23 -0500

    set output container in initialize method and add it to attr reader

--- time_util/parser.rb
+++ time_util/parser.rb
@@ -6,18 +6,18 @@ require 'date'
 
 class Parser 
 
-	attr_reader :flights_csv, :columns, :rows, :container
+	attr_reader :flights_csv, :columns, :rows, :output
 
 #["Carrier Code", "Date (MM/DD/YYYY)", "Flight Number", "Tail Number", "Origin Airport ", "Scheduled Arrival Time", "Actual Arrival Time", nil]
 	def initialize(path_to_csv)
 		@flights_csv = CSV.read(path_to_csv)
 		@columns = flights_csv.first
 		@rows = flights_csv[1..-1]
+		@output = []
   end
 
 #["UA", "12/31/2013", "1675", "N39461", "TPA", "13:07", "12:59", nil], ["UA", "12/31/2013", "1686", "N24729", "SJU", "19:20", "19:37", nil]
 	def parse_data
-		output = []
 		rows.each do |row|
 			hash = {}
 			row.each_with_index do |attribute, i|
